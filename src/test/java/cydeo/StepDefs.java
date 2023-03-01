5a3d42ed8dbb0b73a8039f8ea3482bf5756374be
fatmakayhan <fatmakayhan2018@gmail.com>
2023-03-01 20:59:58 +0100

    @wip added to features

--- src/test/java/cydeo/StepDefs.java
+++ src/test/java/cydeo/StepDefs.java
@@ -17,12 +17,12 @@ import java.util.concurrent.TimeUnit;
 
 
 public class StepDefs {
-
-    @Given("^I am on the home page$")
-    public void i_am_on_the_home_page() throws Throwable {
-        Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        Driver.getDriver().manage().window().maximize();
-        Driver.getDriver().get("http://etsy.com");
+    
+        @Given("^I am on the home page$")
+        public void i_am_on_the_home_page() throws Throwable {
+            Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            Driver.getDriver().manage().window().maximize();
+            Driver.getDriver().get("http://etsy.com");
 
     }
 
